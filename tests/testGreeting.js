b68d61b3b0e0fe38c8069e3396cc9f7da7f2dd87
Penry Downey <penry_downey@hotmail.co.uk>
2020-07-20 14:48:11 +0100

    added new tests and build instructs

--- tests/testGreeting.js
+++ tests/testGreeting.js
@@ -2,6 +2,13 @@ const { describe } = require("mocha");
 const assert = require("assert");
 
 const greeting = require("../lib/greeting");
+const math = require("../math");
+
+describe("Greeting Test Suite:", function () {
+  it("Given the name John, greeting should return hello John", function () {
+    assert(greeting("John") === "Hello John");
+  });
+});
 
 describe("Simple test suite:", function () {
   it("Given the name John, greeting should return hello John", function () {
