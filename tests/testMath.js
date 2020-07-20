cc8992bd177daa4c3a578d6e43e6bb9e31c65f6d
Penry Downey <penry_downey@hotmail.co.uk>
2020-07-20 15:26:52 +0100

    remove bad tests

--- tests/testMath.js
+++
@@ -1,9 +0,0 @@
-const { describe } = require("mocha");
-const assert = require("assert");
-const math = require("../lib/math");
-
-describe("Math test suite:", function () {
-  it("Given two numbers, the function math should return the sum", function () {
-    assert(math(5, 7) === 12);
-  });
-});
